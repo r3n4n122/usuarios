49c084e364fb5d58f66a8afce194eefa1ad3e2f7
r3n4n122 <renan.santos@walljobs.com.br>
2024-12-28 00:27:13 -0300

    first commit

---
+++ front/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ // Caso tenha um arquivo CSS  // O componente App, que você criará
+
+ReactDOM.render(
+  <React.StrictMode>
+
+  </React.StrictMode>,
+  document.getElementById('root')
+);
